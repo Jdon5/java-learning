581be93f920ee88e44a85553dc0f5e41e0897ed1
Jdon5 <162381746+Jdon5@users.noreply.github.com>
2026-02-10 17:56:38 +0100

    feat(kap02): add binary and hexadecimal literal examples

---
+++ kap02/exercises/Zahlen.java
@@ -0,0 +1,14 @@
+public class Zahlen {
+    public static void main(String[] args) {
+
+        // assigned binary number to an int-variable
+        int binary = 0b10101010;
+
+        // assigned hexadecimal number to an int variable
+        int hexa = 0xAA00;
+
+        // print converted values to the console
+        System.out.println("Binary: 10101010 -> Decimal: " + binary);
+        System.out.println("Hexadecimal: 0xAA00 -> Decimal: "+ hexa);
+    }
+}
