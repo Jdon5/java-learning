c0fcefff2237bf026ccfd07c9c36a0f035e6017c
Jdon5 <162381746+Jdon5@users.noreply.github.com>
2026-02-10 18:28:14 +0100

    feat(kap02) add binary and hexadecimal literals  example

---
+++ kap02/exercises/Var.java
@@ -0,0 +1,14 @@
+public class Var {
+    public static void main(String[] args) {
+
+        // assign binary number to var-variable
+        var binary = 0b10101010;
+
+        // assign hexadecimal number to var-variable
+        var hexadecimal = 0xAA00;
+
+        // Print converted number to console
+        System.out.println("Binary: 10101010 -> Decimal: " + binary);
+        System.out.println("Hexadecimal: 0xAA00 -> Decimal: "+ hexadecimal);
+    }
+}
