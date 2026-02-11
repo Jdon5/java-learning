e8c036918c5d477cc9a52d83e232a8c1353f0cdb
Jdon5 <162381746+Jdon5@users.noreply.github.com>
2026-02-11 17:27:48 +0100

    solve divisibility exercise

---
+++ kap03/exercises/Uhrzeit.java
@@ -0,0 +1,15 @@
+public class Uhrzeit {
+    public static void main(String[] args) {
+
+        // assigning literals to variables
+        int x = 12;
+        int y = 5;
+
+        // printing out to console, if x is divisble by y
+        if(x % y == 0){
+            System.out.println(x+" ist durch "+y+" teilbar" );
+        } else {
+            System.out.println(x+" ist nicht durch "+y+" teilbar" );
+        }
+    }
+}
